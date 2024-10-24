f9ac93de7d24032cd9e42844dcf939a744eaa168
kyoon12 <jhonlloydsoco.202300134@gmail.com>
2024-10-24 05:04:33 +0000

    upds

---
+++ js.js
@@ -0,0 +1,23 @@
+(function() {
+    emailjs.init("service_bztndao"); 
+})();
+
+function sendEmail(event) {
+    event.preventDefault();
+
+    const form = document.getElementById('contact-form');
+    const data = {
+        name: form.name.value,
+        email: form.email.value,
+        subject: form.subject.value,
+        message: form.message.value,
+    };
+
+    emailjs.send('service_bztndao', 'template_0qv83tk', data)
+        .then(() => {
+            alert('Your message has been sent successfully!');
+            form.reset();
+        }, (error) => {
+            alert('Failed to send your message. Please try again later.');
+        });
+}
